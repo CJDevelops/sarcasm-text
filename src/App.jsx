51c7f97f17d7448a435bcc49bdd811dd4e1cfd31
Connor Jones <3439843+CJDevelops@users.noreply.github.com>
2023-04-14 13:22:34 +0100

    Uppercase toggle

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import "./App.css";
 
 function App() {
 	const [input, setInput] = useState("");
+	const [startUppercase, setStartUppercase] = useState(true);
 
-	const toSarcasmCase = (text) => {
+	const toSarcasmCase = (text, startUppercase) => {
 		let sarcasmText = "";
-		let isUppercase = true;
+		let isUppercase = startUppercase;
 
 		for (const char of text) {
 			if (char.match(/[a-zA-Z]/)) {
@@ -20,14 +21,25 @@ function App() {
 	};
 
 	const handleInputChange = (event) => {
-		setInput(toSarcasmCase(event.target.value));
+		setInput(toSarcasmCase(event.target.value, startUppercase));
+	};
+
+	const handleCheckboxChange = (event) => {
+		setStartUppercase(event.target.checked);
+		setInput(toSarcasmCase(input, event.target.checked));
 	};
 
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h1>Sarcasm Case Converter</h1>
-				<input type="text" value={input} onChange={handleInputChange} placeholder="Type here..." />
+				<div>
+					<input type="text" value={input} onChange={handleInputChange} placeholder="Type here..." />
+				</div>
+				<div>
+					<span>Start Upper</span>
+					<input type="checkbox" checked={startUppercase} onChange={handleCheckboxChange} style={{ marginLeft: "1rem" }} />
+				</div>
 				<p>{input}</p>
 			</header>
 		</div>
